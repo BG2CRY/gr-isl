83d7035e2315b7ec6b2cb5091f261f80787a4aa7
bg2bhc <bg2bhc@gmail.com>
2018-04-01 13:42:45 +0800

    add c1 sys block delete build

--- build/docs/doxygen/html/api_8h.js
+++
@@ -1,4 +0,0 @@
-var api_8h =
-[
-    [ "ISL_API", "api_8h.html#aab98974db42e532c0fc2e70384fad458", null ]
-];
--- build/docs/doxygen/html/classgr_1_1isl_1_1oqpsk__coherrent__demod__impl.js
+++
@@ -1,7 +0,0 @@
-var classgr_1_1isl_1_1oqpsk__coherrent__demod__impl =
-[
-    [ "oqpsk_coherrent_demod_impl", "classgr_1_1isl_1_1oqpsk__coherrent__demod__impl.html#a5d41b52e166031645788ce80d36dffac", null ],
-    [ "~oqpsk_coherrent_demod_impl", "classgr_1_1isl_1_1oqpsk__coherrent__demod__impl.html#af05e7777b6627ea2a913456e5635ce99", null ],
-    [ "forecast", "classgr_1_1isl_1_1oqpsk__coherrent__demod__impl.html#aebc71fa2ed690805e67a57638738fe3a", null ],
-    [ "general_work", "classgr_1_1isl_1_1oqpsk__coherrent__demod__impl.html#a0ece1a8e33777710208c7d3284fcf84f", null ]
-];
